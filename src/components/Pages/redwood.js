057d5a87cb50bb1e60fdd3cc1fff0c05d9a87144
tarun.varma <tarun.varma@effigoglobal.com>
2024-06-01 13:22:42 +0530

    change commit

--- src/components/Pages/redwood.js
+++ src/components/Pages/redwood.js
@@ -13,6 +13,7 @@ import { useNavigate } from 'react-router-dom';
 import SecureLS from 'secure-ls';
 import ReactPlayer from 'react-player';
 import { TextField } from '@mui/material';
+import Navbar from './Navbar';
 
 const navItems = ['Hello Monday', 'Prometheus Fuels', 'Redwood Empire', 'Log Out'];
 const ls = new SecureLS({ encodingType: 'aes', isCompression: false });
@@ -29,117 +30,19 @@ const ls = new SecureLS({ encodingType: 'aes', isCompression: false });
         setMobileOpen(!mobileOpen);
     };
 
-    //   const handleLogout = () => {
-    //     ls.remove('authenticated');
-    //     Navigate('/login');
-    //     window.location.reload();
-    //   };
-
-    //   const handleHelloMonday = () => {
-    //     Navigate('/hellomonday');
-    //   };
-
-    //   const handlePrometheus = () => {
-    //     Navigate('/prometheus');
-    //   };
-
-    //   const handleRedwood = () => {
-    //     Navigate('/redwood');
-    //   };
-
-    //   const handleHome = () => {
-    //     Navigate('/');
-    //   };
-
-    const getClickHandler = (item) => {
-        switch (item) {
-            case 'Hello Monday':
-                return () => Navigate('/hellomonday');
-            case 'Prometheus Fuels':
-                return () => Navigate('/prometheus');
-            case 'Redwood Empire':
-                return () => Navigate('/redwood');
-            case 'Log Out':
-                return () => {
-                    ls.remove('authenticated');
-                    Navigate('/login');
-                    window.location.reload();
-                };
-            default:
-                return () => Navigate('/');
-        }
-    };
+   
 
     const handleForceRerender = () => {
         // Increment the video key to force a re-render
         setVideoKey(prevKey => prevKey + 1);
     };
 
-    const drawer = (
-        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center', backgroundImage: 'linear-gradient(64.3deg, #001225 6.75%, rgba(0, 13, 27, 0.95) 20.87%, rgba(1, 13, 27, 0.9) 36.96%, rgba(4, 19, 35, 0.83) 52.99%, rgba(0, 0, 0, 0) 91.8%), url(https://s3-alpha-sig.figma.com/img/567f/649b/b308c233645385a9c299e4c895c08787?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Ro6l2~X7NDl0ApHJLT9j3vXtGuaEpTRTQ9y3NpfJQbDbP~HXSH0V85IXCoZRm2TK5Jeohdv50U4QVAFPEennh0k1tC9gL~TUjMIcXyE-84cLqUNlrQCQFkYzC3XkAcevNs4OIPs6uYgtUCEONVtX8WIn~GBAFUQQocesCVHzeSEawdy1vO3lyEhFR09V0LWShjShlAo6ZlzomqHi1PaKyh4WGItO2i5fgto4BTUVjuJSLMrCG5XvvdCuweC0Z27UuOi2sQ3MrixIt~IjOclacuq3AYAiVegHNcuRLBQZs78DFN36QiMihhRvCPM3VTHw0V-4i68vA06kxn0H7AUuFQ__)', height: "100% !important" }}>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1, marginTop: "150px", marginBottom: "50px" }}>
-            <img
-            onClick={() => navigate('/')}
-                src="/assets/Bg.png"
-                alt="EffiGO Logo"
-                style={{ width: 100, height: 'auto', cursor: 'pointer' }}
-            />
-        </Typography>
-        {navItems.map((item) => (
-            <Button key={item} sx={{ width: '100%', color: "#ccc" }} onClick={getClickHandler(item)}>
-                {item}
-            </Button>
-        ))}
-    </Box>
-);
 
 
 
 return (
     <>
-        <Box sx={{ display: 'flex' }}>
-            <AppBar position="static" sx={{ backgroundImage: 'linear-gradient(64.3deg, #001225 6.75%, rgba(0, 13, 27, 0.95) 20.87%, rgba(1, 13, 27, 0.9) 36.96%, rgba(4, 19, 35, 0.83) 52.99%, rgba(0, 0, 0, 0) 91.8%), url(/assets/Bg.png)' }}>
-                    <Toolbar>
-                        {isSmallScreen && (
-                            <IconButton
-                                color="inherit"
-                                aria-label="open drawer"
-                                edge="start"
-                                onClick={handleDrawerToggle}
-                                sx={{ mr: 2 }}
-                            >
-                                <MenuIcon />
-                            </IconButton>
-                        )}
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            <img
-                            onClick={() => navigate('/')}
-                                src="/assets/logo.png"
-                                alt="EffiGO Logo"
-                                style={{ width: 100, height: 'auto', cursor: 'pointer' }}
-                            />
-                        </Typography>
-                        {!isSmallScreen && (
-                            <Box sx={{ display: 'flex', gap: 2 }}>
-                                {navItems.map((item) => (
-                                    <Button key={item} style={{ color: "#fff" }} onClick={getClickHandler(item)}>
-                                        {item}
-                                    </Button>
-                                ))}
-                            </Box>
-                        )}
-                    </Toolbar>
-                </AppBar>
-                <Drawer
-                    anchor="left"
-                    open={mobileOpen}
-                    onClose={handleDrawerToggle}
-                    sx={{ display: { xs: 'block', sm: 'none' } }}
-                >
-                    {drawer}
-                </Drawer>
-
-            </Box>
+        <Navbar/>
             <div style={{background:"#000"}}>
             <Button style={{color:"#fff", fontSize:"12px", border:"1px solid #ccc"}} onClick={handleForceRerender} >
                 Force Re-render
